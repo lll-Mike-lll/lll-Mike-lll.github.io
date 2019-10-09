4b5b3d660c3142fa88a787b7a7c6ed0c4dc0d31c
lll-Mike-lll <wuttinun.code@gmail.com>
2019-10-09 20:11:13 +0700

    m

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -8,7 +8,10 @@ var numberOfDrumButton = document.querySelectorAll(".drum").length;
 for (var i = 0; i < numberOfDrumButton; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click",
     function() {
-      alert("mike");
+      // console.log(this.innerHTML);
+      this.style.color="white";
     }
   );
 }
+// var aud = new Audio("./sounds/tom-1.mp3");
+// aud.play();
