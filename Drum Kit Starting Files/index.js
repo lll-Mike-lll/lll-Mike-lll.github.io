3dbff8543467d1b4ed421bdc9fc86d1493a9137c
lll-Mike-lll <37527876+lll-Mike-lll@users.noreply.github.com>
2019-10-11 10:23:28 +0700

    m

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -32,11 +32,11 @@ for (var i = 0; i < numberOfDrumButton; i++) {
           var aud_j = new Audio("./sounds/tom-2.mp3");
           aud_j.play()
           break;
-          case "k":
+        case "k":
           var aud_k = new Audio("./sounds/tom-3.mp3");
           aud_k.play()
           break;
-          case "l":
+        case "l":
           var aud_l = new Audio("./sounds/tom-4.mp3");
           aud_l.play()
           break;
@@ -45,5 +45,40 @@ for (var i = 0; i < numberOfDrumButton; i++) {
       }
     });
 }
+
+document.addEventListener('keypress', function(event) {
+  console.log(event);
+  switch (event.key) {
+    case "w":
+      var crash = new Audio("./sounds/crash.mp3");
+      crash.play()
+      break;
+    case "a":
+      var kick = new Audio("./sounds/kick-bass.mp3");
+      kick.play()
+      break;
+    case "s":
+      var aud = new Audio("./sounds/snare.mp3");
+      aud.play()
+      break;
+    case "d":
+      var aud_d = new Audio("./sounds/tom-1.mp3");
+      aud_d.play()
+      break;
+    case "j":
+      var aud_j = new Audio("./sounds/tom-2.mp3");
+      aud_j.play()
+      break;
+    case "k":
+      var aud_k = new Audio("./sounds/tom-3.mp3");
+      aud_k.play()
+      break;
+    case "l":
+      var aud_l = new Audio("./sounds/tom-4.mp3");
+      aud_l.play()
+      break;
+    default:}
+});
+
 // var aud = new Audio("./sounds/tom-1.mp3");
 // aud.play();
